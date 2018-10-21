dec3e9399273ee679c061c1a5d3d9d483484a5e9
jamiejamiebobamie <jmccrory@vt.edu>
2018-10-20 20:58:59 -0700

    Program is now correctly giving probability of next word

--- word_probability.py
+++ word_probability.py
@@ -1,7 +1,8 @@
 
 from collections import deque
 import sys
-
+import string
+import re
 
 #w = deque('ghi') adds g, h, i to the the queue
 
@@ -42,30 +43,50 @@ def arrayFileWords(file):
 #            instances.append(instance)
 #    print(instances)
 
+def strip_Punc():
+    punctuation = ["!", "@" , "#" , "$" , "." , ","]
+    for i, word in enumerate(fileWords):
+        newWord = ""
+        for char in word:
+            if char not in punctuation:
+                newWord += char
+        fileWords[i] = newWord
+
+
+fileWords = arrayFileWords(file)
+fileWords = [x.lower() for x in fileWords] #a lit comprehension that lowercases everything ... reconsider where this is in the code structure
+fileWordCount = len(arrayFileWords(file))
+strip_Punc()
 
 def array_of_N_Words_After():
     instances = []
     for i, fileWord in enumerate(fileWords):
         if fileWord == str(word):
             x = i -1
-            afterWords = []
+            beforeWords = []
             while x > (i - int(n) - 1):
-                afterWords.append(fileWords[x])
+                beforeWords.append(fileWords[x])
                 x -= 1
-            myTuple = (word, afterWords, fileWords[i+1])
+            myTuple = (word, beforeWords, fileWords[i+1])
             instances.append(myTuple)
     return instances
 
-fileWords = arrayFileWords(file)
-fileWordCount = len(arrayFileWords(file))
 instances = array_of_N_Words_After()
 
+#myDict_beforeWords = {}
+#myArray_afterWords = []
 myDict = {}
 for i, instance in enumerate(instances):
     if instance[2] not in myDict:
         myDict[instances[i][2]] = 1
     else:
         myDict[instances[i][2]] += 1
+#   if instance[2] not in myArray_afterWords and instance[1] not in myDict_beforeWords:                    ...this is the right idea?
+
+#        myDict_beforeWords[instances[i][2]] = 1
+#        myArray_afterwords.append(instances[i][1])
+#    else:
+#        myDict_beforeWords[instances[i][2]] += 1
 
 keys = list(myDict.keys())
 values = list(myDict.values())
@@ -75,7 +96,7 @@ for i, key in enumerate(keys):
     if i == 0:
         print("Yo girl if you say \'" + str(word) + "\', there is a " + str(int(values[i] / sum(values)*100)) + " percent chance that you're going to say \'" + keys[i] + "\'")
     else:
-        print("and a " + str(values[i] / sum(values)) + " percent chance that you're going to say \'" + keys[i] + "\'.")
+        print("and a " + str(int(values[i] / sum(values)*100)) + " percent chance that you're going to say \'" + keys[i] + "\'.")
 
 #To do:
 
@@ -83,8 +104,10 @@ for i, key in enumerate(keys):
 -sanitzie the input :: stringify it, strip it of punctuation and make everything lowercase
 
 -ensure edge cases of input n are handled (index error)
+...am I okay with it looping? (taping the beginning to the end)
 
--look at the above for-loop and determine why myDict[instances[i][2]] isn't registering different outputs for input "hungry"
+-look at the above for-loop and determine why myDict[instances[i][2]] isn't registering different outputs for input "hungry" (repeated words)
+...it's skipping the first repeated "hungry"'s (2 of them) because they're uppercase...
 """
 
 #accoutn for edge cases where the word is not in the file, or for when the n causes the program to loop over the file lining up the beginning to the end
