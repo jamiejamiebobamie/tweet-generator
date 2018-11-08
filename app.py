7cfbc509ed67bd4f2a19cc0d62897318f26b86d7
jamiejamiebobamie <jmccrory@vt.edu>
2018-11-07 20:28:32 -0800

    app.py before push to heroku

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+app = Flask(__name__)
+from histogram import countWordsDict, countWordsArray, arrayFileWords, strip_Punc, lowercaseArray, keysAsValues
+from stochasticFILE import weightedWord
+import random
+
+def chooseFile():
+    files = ["Herbert.md", "Wilde.md", "Shakespeare.md", "Grimm.md"]
+    return files[random.randint(0,3)]
+
+
+#file = chooseFile()
+#new = lowercaseArray(strip_Punc(arrayFileWords(file)))
+#myArr = weightedWord(countWordsDict(new))
+#print(myArr, file)
+
+
+
+@app.route('/')
+def refesh():
+    file = chooseFile()
+    #new = lowercaseArray(strip_Punc(arrayFileWords(file)))
+    new = arrayFileWords(file)
+    count = 0
+    stri = ""
+    while count < 7:
+        myArr = weightedWord(countWordsDict(new))
+        stri += myArr
+        stri += " "
+        count += 1
+    return "From " + file + ": \'" + stri + "\'"
